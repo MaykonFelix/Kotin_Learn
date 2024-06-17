c65579c2b0ad24b5546d89c30724b32749004c01
Maykon Felix <138487449+MaykonFelixWD@users.noreply.github.com>
2024-06-16 23:25:18 -0300

    :sparkles: remove model

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,10 +24,6 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.boot:spring-boot-starter-validation:3.3.0")
 
-	implementation("com.h2database:h2:2.1.214")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.0.5")
-
-
 	// Test Dependence
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
